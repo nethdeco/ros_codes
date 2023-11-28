da89c16e5bc137fb05c6090a090cc264c2f3bcee
nethdeco <deneth@seaos.co.jp>
2023-11-28 17:36:41 +0900

    add rosbag recrods

---
+++ record_1m_lidar_scans_cam.sh
@@ -0,0 +1,1 @@
+rosbag record --repeat-latched --duration=1m /velodyne_points /wheels_controller/odom /imu/data /camera_front/rgb/camera_info /camera_front/rgb/image_rect_color /tf /tf_static /scan_front
